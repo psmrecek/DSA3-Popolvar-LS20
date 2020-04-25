b926a4c548fb3425c177fde2c5e8e4bab2b8b4a6
Peter Smrecek <petersmrecek@gmail.com>
2020-04-25 18:37:13 +0200

    The Lord of the Binary Heaps: The Fellowship of the Int

--- Popolvar0.c
+++ Popolvar0.c
@@ -1,83 +1,143 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define INF 2147483647
+
+struct Vertex {
+	int x, y;				// index do reprezentacie grafu
+	char status;			// binarne hodnoty stavu hladania: bit 0: je zabity drak, bit 1: je zachranena 1. princezna; bit 2: 2. princezna; ...; bit 5: 5. princezna
+	int lenght;				// dlzka cesty
+}VERTEX;
+
+int heap[255];				// minimalna binarna halda / min heap
+							// moze to byt globalna premenna?
+
+							// https://www.youtube.com/watch?v=lAXZGERcDf4
+
+int getParent(int i) {
+	return (i - 1) / 2;
+}
+
+int getLeft(int i) {
+	return i * 2 + 1;
+}
+
+int getRight(int i) {
+	return i * 2 + 2;
+}
+
+void exchange(int* x, int* y) {
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+void fixHeapUp(int **heap, int fix) {						
+	int parent = getParent(fix);
+
+	if (parent < 0)
+		return;
+
+	while (fix != 0 && (*heap)[parent] > (*heap)[fix]) {
+		exchange(&(*heap)[parent], &(*heap)[fix]);
+
+		fix = parent;
+		parent = getParent(fix);
+	}
+}
+
+void insert(int **heap, int *heapSize, int key) {
+	(*heap)[(*heapSize)++] = key;
+	fixHeapUp(&*heap, *heapSize - 1);
+}
+
+void decrease(int** heap, int i, int key) {
+	(*heap)[i] = key;
+	fixHeapUp(*&heap, i);
+}
+
+void heapify(int** heap, int heapSize, int fix) {
+	int left = getLeft(fix);
+	int right = getRight(fix);
+	int min;
+
+	if (left < heapSize && (*heap)[left] < (*heap)[fix])
+		min = left;
+	else
+		min = fix;
+
+	if (right < heapSize && (*heap)[right] >(*heap)[min])
+		min = right;
+	if (min != fix) {
+		exchange(&(*heap)[fix], &(*heap)[min]);
+		heapify(&*heap, heapSize, min);
+	}
+}
+
+int extractMin(int **heap, int *heapSize) {
+	printf("%d\n", *heapSize);
+	(*heapSize)--;
+	if (*heapSize < 0)
+		return INF;
+	if (*heapSize == 0) 
+		return (*heap)[0];
+
+	int min = (*heap)[0];
+	(*heap)[0] = (*heap)[(*heapSize)];
+	heapify(*&heap, *heapSize, 0);
+	return min;
+}
+
+void search(int* heap, int* heapSize) {
+
+}
+
+void printHeap(int* heap, int heapSize) {
+	for (int i = 0; i < heapSize; i++)
+		printf("%d ", heap[i]);
+	printf("\n");
+}
+
+void printMin(int** heap, int *heapSize) {
+	int min = extractMin(&*heap, &*heapSize);
+	printf("Min je %d\n", min);
+	printHeap(*heap, *heapSize);
+}
+
+
+void transform(char** mapa) {
+
+}
 
 int* zachran_princezne(char** mapa, int n, int m, int t, int* dlzka_cesty) {
+	// Zadanim pozadovana funkcia
+	int *minHeap = NULL, * path = NULL;
+	*dlzka_cesty = 0;
 
+	return NULL;
 }
 
 int main()
 {
-	char** mapa;
-	int i, test, dlzka_cesty, cas, * cesta;
-	int n = 0, m = 0, t = 0;
-	FILE* f;
-	while (1) {
-		printf("Zadajte cislo testu (0 ukonci program):\n");
-		scanf("%d", &test);
-		dlzka_cesty = 0;
-		n = m = t = 0;
-		switch (test) {
-		case 0://ukonci program
-			return 0;
-		case 1://nacitanie mapy zo suboru
-			f = fopen("test.txt", "r");
-			if (f)
-				fscanf(f, "%d %d %d", &n, &m, &t);
-			else
-				continue;
-			mapa = (char**)malloc(n * sizeof(char*));
-			for (i = 0; i < n; i++) {
-				mapa[i] = (char*)malloc(m * sizeof(char));
-				for (int j = 0; j < m; j++) {
-					char policko = fgetc(f);
-					if (policko == '\n') policko = fgetc(f);
-					mapa[i][j] = policko;
-				}
-			}
-			fclose(f);
-			cesta = zachran_princezne(mapa, n, m, t, &dlzka_cesty);
-			break;
-		case 2://nacitanie preddefinovanej mapy
-			n = 10;
-			m = 10;
-			t = 12;
-			mapa = (char**)malloc(n * sizeof(char*));
-			mapa[0] = "CCHCNHCCHN";
-			mapa[1] = "NNCCCHHCCC";
-			mapa[2] = "DNCCNNHHHC";
-			mapa[3] = "CHHHCCCCCC";
-			mapa[4] = "CCCCCNHHHH";
-			mapa[5] = "PCHCCCNNNN";
-			mapa[6] = "NNNNNHCCCC";
-			mapa[7] = "CCCCCPCCCC";
-			mapa[8] = "CCCNNHHHHH";
-			mapa[9] = "HHHPCCCCCC";
-			cesta = zachran_princezne(mapa, n, m, t, &dlzka_cesty);
-			break;
-		case 3: //pridajte vlastne testovacie vzorky
-		default:
-			continue;
-		}
-		cas = 0;
-		for (i = 0; i < dlzka_cesty; i++) {
-			printf("%d %d\n", cesta[i * 2], cesta[i * 2 + 1]);
-			if (mapa[cesta[i * 2 + 1]][cesta[i * 2]] == 'H')
-				cas += 2;
-			else
-				cas += 1;
-			if (mapa[cesta[i * 2 + 1]][cesta[i * 2]] == 'D' && cas > t)
-				printf("Nestihol si zabit draka!\n");
-			if (mapa[cesta[i * 2 + 1]][cesta[i * 2]] == 'N')
-				printf("Prechod cez nepriechodnu prekazku!\n");
-			if (i > 0 && abs(cesta[i * 2 + 1] - cesta[(i - 1) * 2 + 1]) + abs(cesta[i * 2] - cesta[(i - 1) * 2]) > 1)
-				printf("Neplatny posun Popolvara!\n");
-		}
-		printf("%d\n", cas);
-		free(cesta);
-		for (i = 0; i < n; i++) {
-			free(mapa[i]);
-		}
-		free(mapa);
-	}
+	int capacity = 100;
+	int *heap = malloc(capacity * sizeof(int));
+	int heapSize = 0;
+
+	insert(&heap, &heapSize, 5);
+	printHeap(heap, heapSize);
+
+	insert(&heap, &heapSize, 4);
+	printHeap(heap, heapSize);
+
+	insert(&heap, &heapSize, 3);
+	printHeap(heap, heapSize);
+
+	insert(&heap, &heapSize, 2);
+	printHeap(heap, heapSize);
+
+	decrease(&heap, 2, -1);
+	printHeap(heap, heapSize);
+
+	printMin(&heap, &heapSize);
+
 	return 0;
 }
