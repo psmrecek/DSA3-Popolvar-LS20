a0bd50fc99ded5f708e251b689232d544da4a199
Peter Smrecek <petersmrecek@gmail.com>
2020-05-01 23:05:03 +0200

    Pridana zachrana 1 princeznej a vygenerovanie matice susednosti pre novy graf v ktorom sa bude hladat najlacnejsia Hamiltonovska cesta.

--- Popolvar2.c
+++ Popolvar2.c
@@ -4,11 +4,17 @@
 
 typedef struct vertex {
 	int x, y;				// indexy do reprezentacie grafu
-	char status;			// binarne hodnoty stavu hladania: bit 0: je zabity drak, bit 1: je zachranena 1. princezna; bit 2: 2. princezna; ...; bit 5: 5. princezna
-	int lenght;				// dlzka cesty
 	int parentX, parentY;	// indexy na rodica
-	char type;
+	int lenght;				// dlzka cesty
 }VERTEX;
+
+typedef struct princess {
+	int x, y;
+	VERTEX *heap;
+	int **mapHeap;
+	int heapSize;
+	int n;
+}PRINCESS;
 
 int heap[255];				// minimalna binarna halda / min heap
 							// moze to byt globalna premenna?
@@ -42,12 +48,6 @@ void exchange(VERTEX* a, VERTEX* b, int*** mapHeap) {
 	*a = *b;
 	*b = temp;
 }
-
-//void exchange(VERTEX* a, VERTEX* b) {
-//	VERTEX temp = *a;
-//	*a = *b;
-//	*b = temp;
-//}
 
 void fixHeapUp(VERTEX** heap, int fix, int*** mapHeap) {
 	int parent = getParent(fix);
@@ -117,13 +117,13 @@ VERTEX extractMin(VERTEX** heap, int* heapSize, int*** mapHeap) {
 
 void printHeap(VERTEX* heap, int heapSize, int*** mapHeap) {
 	for (int i = 0; i < heapSize; i++)
-		printf("[%d %d] %d %c | Poradie %d = %d mapHeap\n", heap[i].x, heap[i].y, heap[i].lenght, heap[i].type, i, (*mapHeap)[heap[i].x][heap[i].y]);
+		printf("[%d %d] %d | Poradie %d = %d mapHeap\n", heap[i].x, heap[i].y, heap[i].lenght, i, (*mapHeap)[heap[i].x][heap[i].y]);
 	printf("\n");
 }
 
 void printExtractMin(VERTEX** heap, int* heapSize, int*** mapHeap) {
 	VERTEX min = extractMin(&*heap, &*heapSize, &(*mapHeap));
-	printf("[%d %d] %d %c | HeapSize %d = %d Poloha\n", min.x, min.y, min.lenght, min.type, *heapSize, (*mapHeap)[min.x][min.y]);
+	printf("[%d %d] %d | HeapSize %d = %d Poloha\n", min.x, min.y, min.lenght, *heapSize, (*mapHeap)[min.x][min.y]);
 	printHeap(*heap, *heapSize, &(*mapHeap));
 }
 
@@ -250,9 +250,56 @@ void djikstra(VERTEX** heap, int* heapSize, int*** mapHeap, int n, int m, char *
 	}
 }
 
+void addToArray(int** des, int* src, int desCount, int srcCount) {
+	for (int i = 0; i < srcCount; i++)
+	{
+		(*des)[i + desCount] = src[i];
+	}
+}
+
+void copyHeap(VERTEX *sHeap, VERTEX **dHeap, int **sMapHeap, int ***dMapHeap, int sHeapSize, int *dHeapSize, int n, int m) {
+	int capacity = n * m;
+	*dHeapSize = sHeapSize;
+	VERTEX* finalHeap = malloc(capacity * sizeof(VERTEX));
+	for (int i = 0; i < n * m; i++)
+	{
+		finalHeap[i] = sHeap[i];
+	}
+	int** finalMapHeap = (int**)malloc(n * sizeof(int*));
+	for (int i = 0; i < n; i++) {
+		finalMapHeap[i] = (int*)malloc(m * sizeof(int));
+		for (int j = 0; j < m; j++)
+		{
+			finalMapHeap[i][j] = sMapHeap[i][j];
+		}
+	}
+	*dHeap = finalHeap;
+	*dMapHeap = finalMapHeap;
+}
+
+//int pathCounter(VERTEX *heap, int **mapHeap, int startX, int startY, int endX, int endY) {
+//	int count = 0;
+//	VERTEX temp = heap[mapHeap[endX][endY]];
+//	while (temp.x != startX || temp.y != startY)
+//	{
+//		count++;
+//		if (temp.parentX < 0 || temp.parentY < 0)
+//		{
+//			printf("K princeznej neexistuje cesta!\n");
+//			printf("[%d %d] [%d %d]\n", startX, startY, endX, endY);
+//			return 0;
+//		}
+//		else
+//		{
+//			temp = heap[mapHeap[temp.parentX][temp.parentY]];
+//		}
+//	}
+//	return count;
+//}
+
 int* zachran_princezne(char** mapa, int n, int m, int t, int* dlzka_cesty) {
 	// Zadanim pozadovana funkcia
-	int* path = malloc(1000*sizeof(int));						// Dlzku treba ale nastavovat dynamicky
+
 	*dlzka_cesty = 0;
 
 	int capacity = n * m;
@@ -260,12 +307,13 @@ int* zachran_princezne(char** mapa, int n, int m, int t, int* dlzka_cesty) {
 
 	VERTEX* heap = malloc(capacity * sizeof(VERTEX));
 	int heapSize = n * m;
+
 	int** mapHeap = (int**)malloc(n * sizeof(int*));
+	for (int i = 0; i < n; i++)
+		mapHeap[i] = (int*)malloc(m * sizeof(int));
 
 	int* princesses = malloc(5 * 2 * sizeof(int));
 	int princessCounter = 0;
-	for (int i = 0; i < n; i++)
-		mapHeap[i] = (int*)malloc(m * sizeof(int));
 
 	int dragonX, dragonY;
 		for (int i = 0; i < n; i++)
@@ -274,9 +322,8 @@ int* zachran_princezne(char** mapa, int n, int m, int t, int* dlzka_cesty) {
 				heap[index].x = i;
 				heap[index].y = j;
 				heap[index].lenght = INF;
-				heap[index].parentX = NULL;
-				heap[index].parentY = NULL;
-				heap[index].type = mapa[i][j];
+				heap[index].parentX = -1;
+				heap[index].parentY = -1;
 				mapHeap[i][j] = index;
 				if (mapa[i][j] == 'D')
 				{
@@ -302,34 +349,137 @@ int* zachran_princezne(char** mapa, int n, int m, int t, int* dlzka_cesty) {
 				}
 			}
 		}
-		
+
+		//VERTEX* dragonHeap = malloc(capacity * sizeof(VERTEX));
+		//int dragonHeapSize = n * m;
+		//for (int i = 0; i < dragonHeapSize; i++)
+		//{
+		//	dragonHeap[i] = heap[i];
+		//}
+		//dragonHeapSize = heapSize;
+
+		//int** dragonMapHeap = (int**)malloc(n * sizeof(int*));
+		//for (int i = 0; i < n; i++) {
+		//	dragonMapHeap[i] = (int*)malloc(m * sizeof(int));
+		//	for (int j = 0; j < m; j++)
+		//	{
+		//		dragonMapHeap[i][j] = mapHeap[i][j];
+		//	}
+		//}
+
+		VERTEX* originalHeap;
+		int originalHeapSize;
+		int** originalMapHeap;
+		copyHeap(heap, &originalHeap, mapHeap, &originalMapHeap, heapSize, &originalHeapSize, n, m);
+
+		VERTEX* dragonHeap;
+		int dragonHeapSize;
+		int** dragonMapHeap;
+		copyHeap(heap, &dragonHeap, mapHeap, &dragonMapHeap, heapSize, &dragonHeapSize, n, m);
+
 		decrease(&heap, mapHeap[0][0], verticesLen(0, 0, mapa), &mapHeap);
 		djikstra(&heap, &heapSize, &mapHeap, n, m, mapa);
 
-
+		int* path = malloc(10000 * sizeof(int));						// Dlzku treba ale nastavovat dynamicky
 		VERTEX temp = heap[mapHeap[dragonX][dragonY]];
 		int count = 0;
-		while (temp.x != 0 || temp.y != 0)
+		while (1)
 		{
 			path[count++] = temp.x;
 			path[count++] = temp.y;
 
-			temp = heap[mapHeap[temp.parentX][temp.parentY]];
+			if (temp.parentX == -1 || temp.parentY == -1)
+			{
+				break;
+			}
+			else
+			{
+				temp = heap[mapHeap[temp.parentX][temp.parentY]];
+			}	
 		}
-		path[count++] = 0;
-		path[count++] = 0;
+		reverse(&path, count);
 		*dlzka_cesty = count / 2;
 
-		//for (int i = 0; i < n; i++)
-		//{
-		//	//for (int j = 0; j < m; j++)
-		//	//{
-		//	//	printf("%3c", mapa[i][j]);
-		//	//}
-		//	printf("%s\n", mapa[i]);
-		//}
+		if (*dlzka_cesty == 1)
+		{
+			printf("K drakovi neexistuje cesta!\n");
+			*dlzka_cesty = 0;
+			return path;
+		}
 
-		reverse(&path, count);
+
+		decrease(&dragonHeap, dragonMapHeap[dragonX][dragonY], 1, &dragonMapHeap);
+		djikstra(&dragonHeap, &dragonHeapSize, &dragonMapHeap, n, m, mapa);
+
+		if (princessCounter == 1) {
+			int* dragonPath = malloc(1000 * sizeof(int));						// Dlzku treba ale nastavovat dynamicky
+			int count2 = 0;
+			VERTEX temp2 = dragonHeap[dragonMapHeap[princesses[0]][princesses[1]]];
+			while (temp2.x != dragonX || temp2.y != dragonY)
+			{
+				//printf("[%d %d]\n", temp.y, temp.x);
+				dragonPath[count2++] = temp2.x;
+				dragonPath[count2++] = temp2.y;
+
+				if (temp2.parentX < 0 || temp2.parentY < 0)
+				{
+					printf("K princeznej neexistuje cesta!\n");
+					*dlzka_cesty = 0;
+					return path;
+				}
+				else 
+				{
+					temp2 = dragonHeap[dragonMapHeap[temp2.parentX][temp2.parentY]];
+				}
+			}
+			reverse(&dragonPath, count2);
+			addToArray(&path, dragonPath, count, count2);
+			*dlzka_cesty += count2 / 2;
+		}
+		else
+		{
+			PRINCESS* pr = malloc(princessCounter * sizeof(PRINCESS));
+			for (int i = 0; i < princessCounter; i++)
+			{
+				pr[i].n = i;
+				copyHeap(originalHeap, &(pr[i].heap), originalMapHeap, &(pr[i].mapHeap), originalHeapSize, &(pr[i].heapSize), n, m);
+				decrease(&(pr[i].heap), pr[i].mapHeap[princesses[i * 2]][princesses[i * 2 + 1]], 1, &(pr[i].mapHeap));
+				djikstra(&(pr[i].heap), &(pr[i].heapSize), &(pr[i].mapHeap), n, m, mapa);
+			}
+
+			int matrixSize = princessCounter + 1;
+			int** matrix = (int**)malloc(matrixSize * sizeof(int*));
+			for (int i = 0; i < (princessCounter + 1); i++) {
+				matrix[i] = (int*)malloc(matrixSize * sizeof(int));
+				memset(matrix[i], -1, matrixSize * sizeof(int));
+			}
+
+			for (int i = 0; i < princessCounter; i++)
+			{
+				matrix[0][i + 1] = dragonHeap[dragonMapHeap[princesses[i * 2]][princesses[i * 2 + 1]]].lenght;
+			}
+
+			for (int i = 0; i < princessCounter; i++)
+			{
+				for (int j = i + 1; j < princessCounter; j++)
+				{
+					matrix[i+1][j+1] = pr[i].heap[pr[i].mapHeap[princesses[j * 2]][princesses[j * 2 + 1]]].lenght;
+				}
+			}
+
+			for (int i = 0; i < matrixSize; i++)
+			{
+				for (int j = 0; j < matrixSize; j++)
+				{
+					printf("%2d ", matrix[i][j]);
+				}
+				printf("\n");
+			}
+			
+		}
+
+//		printf("%d\n", princessCounter);
+
 		return path;
 }
 
@@ -340,39 +490,39 @@ int main()
 	int n = 0, m = 0, t = 0;
 	FILE* f;
 
-	n = 10;
-	m = 10;
-	t = 12;
-	mapa = (char**)malloc(n * sizeof(char*));
-	mapa[0] = "CCHCNHCCHN";
-	mapa[1] = "NNCCCHHCCC";
-	mapa[2] = "DNCCNNHHHC";
-	mapa[3] = "CHHHCCCCCC";
-	mapa[4] = "CCCCCNHHHH";
-	mapa[5] = "PCHCCCNNNN";
-	mapa[6] = "NNNNNHCCCC";
-	mapa[7] = "CCCCCPCCCC";
-	mapa[8] = "CCCNNHHHHH";
-	mapa[9] = "HHHPCCCCCC";
-	cesta = zachran_princezne(mapa, n, m, t, &dlzka_cesty);
-			
-	//f = fopen("vstup3.txt", "r");
-	//if (f)
-	//	fscanf(f, "%d %d %d", &n, &m, &t);
-	//else
-	//	return 0;
+	//n = 10;
+	//m = 10;
+	//t = 12;
 	//mapa = (char**)malloc(n * sizeof(char*));
-	//for (i = 0; i < n; i++) {
-	//	mapa[i] = (char*)malloc(m * sizeof(char));
-	//	for (int j = 0; j < m; j++) {
-	//		char policko = fgetc(f);
-	//		if (policko == '\n') policko = fgetc(f);
-	//		mapa[i][j] = policko;
-	//	}
-	//}
-	//fclose(f);
+	//mapa[0] = "CCHCNHCCHN";
+	//mapa[1] = "NNCCCHHCCC";
+	//mapa[2] = "DNCCNNHHHC";
+	//mapa[3] = "CHHHCCCCCC";
+	//mapa[4] = "CCCCCNHHHH";
+	//mapa[5] = "PCHCCCNNNN";
+	//mapa[6] = "NNNNNHCCCC";
+	//mapa[7] = "CCCCCPCCCC";
+	//mapa[8] = "CCCNNHHHHH";
+	//mapa[9] = "HHHPCCCCCC";
 	//cesta = zachran_princezne(mapa, n, m, t, &dlzka_cesty);
-
+			
+//	f = fopen("jednaPrincezna.txt", "r");
+	f = fopen("test.txt", "r");
+	if (f)
+		fscanf(f, "%d %d %d", &n, &m, &t);
+	else
+		return 0;
+	mapa = (char**)malloc(n * sizeof(char*));
+	for (i = 0; i < n; i++) {
+		mapa[i] = (char*)malloc(m * sizeof(char));
+		for (int j = 0; j < m; j++) {
+			char policko = fgetc(f);
+			if (policko == '\n') policko = fgetc(f);
+			mapa[i][j] = policko;
+		}
+	}
+	fclose(f);
+	cesta = zachran_princezne(mapa, n, m, t, &dlzka_cesty);
 
 	cas = 0;
 	for (i = 0; i < dlzka_cesty; i++) {
